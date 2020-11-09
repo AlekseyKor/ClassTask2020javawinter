1bedf99e2cf572b4e0f14c71eb02fac51e18a62d
Aleksey Korchynsky <maritimealeksey@gmail.com>
2020-11-09 20:08:43 +0200

    this is NameExample

---
+++ javaClassWork/src/com/hillel/lesson2/NameExample.java
@@ -0,0 +1,21 @@
+package com.hillel.lesson2;
+
+public class NameExample {
+    public static void main(String[] args) {
+        печать();
+        _test();
+        print_test();
+    }
+
+    public static void печать(){
+
+    }
+
+    public static void _test(){
+
+    }
+
+    public static void print_test(){
+        System.out.println("print_test");
+    }
+}
