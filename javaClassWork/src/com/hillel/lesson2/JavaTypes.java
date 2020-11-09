46eaa396357c475065c12ea6ccf843e2c1a024fc
Aleksey Korchynsky <maritimealeksey@gmail.com>
2020-11-09 20:39:05 +0200

    this is java types

---
+++ javaClassWork/src/com/hillel/lesson2/JavaTypes.java
@@ -0,0 +1,24 @@
+package com.hillel.lesson2;
+
+public class JavaTypes {
+    public static void main(String[] args) {
+
+        // -128......127
+        byte bmin = -128;
+        byte bmax = 127;
+
+        char c = 123;
+
+        long l = 100L;
+
+        double d = 12d;
+
+        float f = 12f;
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+
+
+    }
+}
