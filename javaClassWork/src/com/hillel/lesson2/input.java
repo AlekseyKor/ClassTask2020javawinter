9825248ac8ac7a8cda7f967ca861b28af347ad9a
Aleksey Korchynsky <maritimealeksey@gmail.com>
2020-11-09 20:00:47 +0200

    this is input

---
+++ javaClassWork/src/com/hillel/lesson2/input.java
@@ -0,0 +1,9 @@
+package com.hillel.lesson2;
+
+public class input {
+    public static void main(String[] args) {
+        for (String str: args) {
+            System.out.println(str);
+        }
+    }
+}
